88fe1a9a4835045627f2fc59e2b0e7f3524a05b6
Rakshitha Guru Raj <rakshitha27@gmail.com>
2020-12-31 13:26:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,43 +1,14 @@
 #!/bin/bash -e
 
-export PACKAGES="\
-    coreutils \
-    sudo \
-    git \
-    openssh-client \
-    ca-certificates \
-    musl \
-    linux-headers \
-    build-base \
-    python2 \
-    python2-dev \
-    py-pip \
-    wget \
-"
+export NVM_VERSION=v0.35.3
+echo "================= Installing NVM "$NVM_VERSION" ================"
+curl -sS https://raw.githubusercontent.com/creationix/nvm/"$NVM_VERSION"/install.sh | bash
 
-echo "================ installing packages ======================="
-apk update
-apk add --no-cache $PACKAGES
+# Set NVM_DIR so the installations go to the right place
+export NVM_DIR="/root/.nvm"
 
-
-echo "================ installing glibc from sgerrand======================="
-wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-bin-2.27-r0.apk
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk
-
-apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk glibc-i18n-2.27-r0.apk
-/usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
-
-echo "================ make some useful symlinks that are expected to exist ======================="
-if [[ ! -e /usr/bin/python ]];        then ln -sf /usr/bin/python2.7 /usr/bin/python; fi
-if [[ ! -e /usr/bin/python-config ]]; then ln -sf /usr/bin/python2.7-config /usr/bin/python-config; fi
-if [[ ! -e /usr/bin/easy_install ]];  then ln -sf /usr/bin/easy_install-2.7 /usr/bin/easy_install; fi
-
-echo "================ install and upgrade pip ======================="
-easy_install pip
-pip install --upgrade pip
-if [[ ! -e /usr/bin/pip ]]; then ln -sf /usr/bin/pip2.7 /usr/bin/pip; fi
-
-echo "================ test ssh ======================="
-eval `ssh-agent -s`
+NODE_VERSION=8.17.0
+NPM_VERSION=6.14.5
+echo "=============== Installing Node $NODE_VERSION ============="
+. /root/.nvm/nvm.sh && nvm install $NODE_VERSION
+npm install npm@$NPM_VERSION -g
